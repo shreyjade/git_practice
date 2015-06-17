76b5636e1ce9ece3d3847317e9e07bb5cdc34fb4
Shreyansh Jain <shreyansh.jain@gmail.com>
2015-06-17 18:03:48 +0530

    a=10

--- app/src/main/java/com/example/shreyansh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shreyansh/myapplication/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends Activity {
         TextView textViewToChange = (TextView) findViewById(R.id.hey);
         textViewToChange.setText("Good Morning");
 
+        int a = 10;
+
 /*        if (savedInstanceState == null) {
             Logger.log("count : " + i);
         }
